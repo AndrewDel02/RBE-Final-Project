56aee923cfd66e6c741f5e291e7ad9fa6944ebdd
Andrew Del Vecchio <andrewdel82@gmail.com>
2020-05-08 23:45:28 -0400

    setPidSpeed implemented
    motor speeds now fully controlled by pid speed controller

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,39 @@
 #define turn90 500
 #define arbitrarySpeed 20
 #define time360 200
+// one encoder measures faster than the other, use this to compensate
+#define wheelBias 0
+// PID constants
+#define speedKp 12
+#define speedKi 1
 
-enum States {IDLE, WALL_FOLLOW, LINE_FOLLOW, TURN_90, DRIVE_STRAIGHT, SPIN};
-States state = IDLE;
-
+enum States {IDLE, WALL_FOLLOW, LINE_FOLLOW, TURN_90, DRIVE_STRAIGHT, SPIN, TESTING};
+States state = TESTING;
+// instantiate classes
 EventTimer timer;
 Button buttonA(14);
+Zumo32U4Motors motors;
+Zumo32U4Encoders encoders;
 
-void setPidSpeed(int rightSpeed, int leftSpeed);
-float readWallDistance();
-float* wallSpeed(float distance);
-bool lineDetected();
-bool irDetected();
-int* readLineSensor();
-float* lineSpeeds(int lineVals[]);
-bool angleToFlat();
+// declare prototypes
+void setPidSpeed(float targetLeft, float targetRight); // TODO
+float readWallDistance(); // TODO
+float wallSpeed(float distance); // TODO
+bool lineDetected(); // TODO
+bool irDetected(); // TODO
+int readLineSensor(); // TODO
+float lineSpeeds(int lineVals); // TODO
+bool angleToFlat(); // TODO
+void configTimer();
+
+// global variables, try to minimize
+bool readyToPID;
+volatile int16_t countsLeft;
+volatile int16_t countsRight;
 
 void setup() {
-  // put your setup code here, to run once:
   buttonA.Init();
+  configTimer();
 }
 
 void loop() {
@@ -44,9 +58,9 @@ void loop() {
       // measure val
       float distance = readWallDistance();
       // PID calculation here
-      float *speeds = wallSpeed(distance);
+      float speeds = wallSpeed(distance);
       // set PID speed target
-      setPidSpeed(speeds[0], speeds[1]);
+      setPidSpeed(0,0); // placeholder
       // check transition condition
       if (lineDetected()) {
         state = TURN_90;
@@ -56,11 +70,11 @@ void loop() {
 
     case LINE_FOLLOW: {
       // measure val
-      int *lineVals = readLineSensor();
+      int lineVals = readLineSensor();
       // PID calculation here
-      float *speeds = lineSpeeds(lineVals);
+      float speeds = lineSpeeds(lineVals);
       // set PID speed target
-      setPidSpeed(speeds[0], speeds[1]);
+      setPidSpeed(0,0); // placeholder
       // check transition condition
       if (irDetected()) {
         state = TURN_90;
@@ -98,6 +112,91 @@ void loop() {
       }
     }
 
-
+    case TESTING: {
+      static bool run = false;
+      if (buttonA.CheckButtonPress()) run = !run;
+      run ? setPidSpeed(10, 10) : setPidSpeed(0, 0);
+      break;
+    }
   }
+}
+
+void configTimer() {
+  noInterrupts(); //disable interupts while we mess with the Timer4 registers
+  //sets up timer 4
+  TCCR4A = 0x00; // taken from example code, too scared to touch
+  TCCR4B = 0x0C; // prescaler set to 2048
+  TCCR4C = 0x04; // toggles pin 6 at the timer frequency
+  TCCR4D = 0x00; // normal mode
+
+  OCR4C = 141;   // overflow every
+  TIMSK4 = 0x04; //enable overflow interrupt
+
+  interrupts(); //re-enable interrupts
+}
+
+void setPidSpeed(float targetLeft, float targetRight) {
+  if (!readyToPID) return; // bail if not ready to Pid
+  readyToPID = false;
+  // need to correct for difference in wheel encoders, wheelBias determined expirimentally
+  if (targetRight != 0) targetRight += wheelBias;
+  //for tracking previous counts
+  static int16_t prevLeft = 0;
+  static int16_t prevRight = 0;
+  //error sum
+  static int16_t sumLeft = 0;
+  static int16_t sumRight = 0;
+
+  // disable interrupts while reading from volatile variable
+  noInterrupts();
+  int16_t speedLeft = countsLeft - prevLeft;
+  int16_t speedRight = countsRight - prevRight;
+  prevLeft = countsLeft;
+  prevRight = countsRight;
+  interrupts();
+
+  int16_t errorLeft = targetLeft - speedLeft;
+  sumLeft += errorLeft;
+
+  float effortLeft = speedKp * errorLeft + speedKi * sumLeft;
+
+  int16_t errorRight = targetRight - speedRight;
+  sumRight += errorRight;
+
+  float effortRight = speedKp * errorRight + speedKi * sumRight;
+
+  motors.setSpeeds(effortLeft, effortRight); //up to you to add the right motor
+}
+
+float readWallDistance() {
+  return 0.0;
+}
+float wallSpeed(float distance) {
+  return 0.0;
+}
+bool lineDetected() {
+  return false;
+}
+bool irDetected() {
+  return false;
+}
+int readLineSensor() {
+  return 0;
+}
+float lineSpeeds(int lineVals) {
+  return 1.0;
+}
+bool angleToFlat() {
+  return false;
+}
+
+
+// pid timer ISR, set flag and get encoder vals
+ISR(TIMER4_OVF_vect)
+{
+  //Capture a "snapshot" of the encoder counts for later processing
+  countsLeft = encoders.getCountsLeft();
+  countsRight = encoders.getCountsRight();
+
+  readyToPID = true;
 }
